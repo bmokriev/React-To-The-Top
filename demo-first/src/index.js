72f3917574b372730fad7f3be2c30b5c6fbbfbaf
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-16 13:34:37 +0300

    using child component
    using child component

--- demo-first/src/index.js
+++ demo-first/src/index.js
@@ -3,25 +3,35 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 13:44" 
-        comment="Nice post!" 
-        img={faker.image.avatar()}/>
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 14:24" 
-        comment="Great topic!" 
-        img={faker.image.avatar()}/>
-      <CommentDetail 
-        author="Tony" 
-        timeAgo="Today at 14:45" 
-        comment="Amazing content!" 
-        img={faker.image.avatar()}/>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 13:44" 
+          comment="Nice post!" 
+          img={faker.image.avatar()}/>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 14:24" 
+          comment="Great topic!" 
+          img={faker.image.avatar()}/>
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail 
+          author="Tony" 
+          timeAgo="Today at 14:45" 
+          comment="Amazing content!" 
+          img={faker.image.avatar()}/>
+      </ApprovalCard>
+      
     </div>
   );
 }
