8351d134da76f99dab160b110007335a02af2048
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-19 23:06:07 +0300

    setting up SeasonDisplay component
    setting up the component to show different views according to params

--- silly-weather-app/src/index.js
+++ silly-weather-app/src/index.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  state = { lat: null, errorMsg: "" }; 
 
-        this.state = { lat: null, errorMsg: '' };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMsg: err.message })
+    );
+  }
 
-        window.navigator.geolocation.getCurrentPosition(
-          (position) => {
-            this.setState({ lat: position.coords.latitude });
-          },
-          (err) => {
-              this.setState({ errorMsg: err.message })
-        }
-        );
+  //React says we have to define render
+  render() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+    if (!this.state.errorMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat } />
     }
 
-    //React says we have to define render
-    render() {
-        if (this.state.errorMsg && !this.state.lat) {
-            return <div>Error: {this.state.errorMsg}</div>
-        }
-        if (!this.state.errorMsg && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
-        }
-
-        return <div>Loading...</div>
-    };
+    return <div>Loading...</div>;
+  }
 }
-
+ 
 ReactDom.render(
     <App />,
     document.querySelector('#root')
