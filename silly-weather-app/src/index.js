fe8f7ecb5c2b35965b881e731951f80fd5fce1c9
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-19 20:24:15 +0300

    Error handling
    Simple error handling with if statements

--- silly-weather-app/src/index.js
+++ silly-weather-app/src/index.js
@@ -5,19 +5,28 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMsg: '' };
 
         window.navigator.geolocation.getCurrentPosition(
           (position) => {
             this.setState({ lat: position.coords.latitude });
           },
-          (err) => this.setState({ lat: 'Error' })
+          (err) => {
+              this.setState({ errorMsg: err.message })
+        }
         );
     }
 
     //React says we have to define render
     render() {
-    return <div>Latitude: {this.state.lat}</div>;
+        if (this.state.errorMsg && !this.state.lat) {
+            return <div>Error: {this.state.errorMsg}</div>
+        }
+        if (!this.state.errorMsg && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>
     };
 }
 
