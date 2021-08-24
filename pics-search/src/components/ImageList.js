54b67a7088a278c6fb3a1f3258c20f745403e226
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-24 17:21:26 +0300

    adding alt description

--- pics-search/src/components/ImageList.js
+++ pics-search/src/components/ImageList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  console.log(props.images);
-  return <div>ImageList</div>;
+  console.log(props);
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} alt={image.alt_description} />;
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
