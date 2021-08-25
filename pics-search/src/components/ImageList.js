d1717fcff7e82b57830459f6795c73f53f374501
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-25 18:23:49 +0300

    refactoring inside ImageList component

--- pics-search/src/components/ImageList.js
+++ pics-search/src/components/ImageList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  console.log(props);
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} alt={image.alt_description} />;
+  const images = props.images.map(({ alt_description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={alt_description} />;
   });
 
   return <div>{images}</div>;
