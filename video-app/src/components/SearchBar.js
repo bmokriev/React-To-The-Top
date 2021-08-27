6d6167964bdc7c20500e400f7b254f6da7ec97dd
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-27 17:29:01 +0300

    Adding search bar

--- video-app/src/components/SearchBar.js
+++ video-app/src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar works</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label for="search-bar">Video Search</label>
+            <input type="text" id="search-bar"></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
