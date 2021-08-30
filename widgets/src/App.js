5a06f06af26638355287aea3cf6e757be4f8b091
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-08-30 23:03:27 +0300

    Adding Search component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript library',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React works and is simple. People like React.',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'React works by creating components.',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript library',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React works and is simple. People like React.',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'React works by creating components.',
+//   },
+// ];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
