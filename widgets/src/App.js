6e57eff50b2ca0b73e9e04e1af94935fe226e554
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-09-01 21:17:15 +0300

    Dropdown component initial setup

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+
 // import Accordion from './components/Accordion';
-import Search from './components/Search';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 // const items = [
 //   {
@@ -17,10 +19,25 @@ import Search from './components/Search';
 //   },
 // ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue',
+  },
+];
+
 const App = () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
