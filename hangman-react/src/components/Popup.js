e2e3c2e9cb68b563fdb331556d63bb5f52354719
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-09-14 22:59:28 +0300

    Create Popup.js

---
+++ hangman-react/src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Popup = () => {
+  return (
+    <div class="popup-container" id="popup-container">
+      <div class="popup">
+        <h2 id="final-message"></h2>
+        <button id="play-button">Play Again</button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
