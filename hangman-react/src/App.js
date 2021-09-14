6d02d34872bd4f11a7a4dc2d1e8c727047644e22
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-09-14 21:09:51 +0300

    initial setup

---
+++ hangman-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Figure from './components/Figure';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
