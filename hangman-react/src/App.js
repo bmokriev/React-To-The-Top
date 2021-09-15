7541837b06164bbdb59ad81f7a41b03e6684b3f6
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-09-15 18:37:51 +0300

    Display word setup

--- hangman-react/src/App.js
+++ hangman-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
@@ -6,14 +6,22 @@ import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 // import Popup from './components/Popup';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setВрongLetters] = useState([]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
