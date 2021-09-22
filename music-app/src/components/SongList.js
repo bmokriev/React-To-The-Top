2f396335a37640a5fc4e71b6ff3ca003e1e6e27f
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-09-22 20:54:12 +0300

    Creating a list of songs an styling it

--- music-app/src/components/SongList.js
+++ music-app/src/components/SongList.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+  renderList() {
+    // console.log(this.props);
+    // return <div>ppp</div>;
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -11,4 +26,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
